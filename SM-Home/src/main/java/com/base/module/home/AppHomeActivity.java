c3b06f585968f6d5566e0ef01aaf7d4b19fc49bc
reber <reber2c@163.com>
2020-06-13 14:38:22 +0800

    create activity file between home and orders modules

---
+++ SM-Home/src/main/java/com/base/module/home/AppHomeActivity.java
@@ -0,0 +1,17 @@
+package com.base.module.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author reber
+ */
+public class AppHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ SM-Orders/src/main/java/com/base/module/order/AppOrderActivity.java
@@ -0,0 +1,14 @@
+package com.base.module.order;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AppOrderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_order);
+    }
+}
